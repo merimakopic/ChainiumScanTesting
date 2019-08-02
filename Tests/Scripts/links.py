47cbbd4059ff44f317e2e9ebf70f347795566f84
merimakopic <merima.kopic@fet.ba>
2019-08-03 01:18:41 +0200

    script for number of links and texts

--- Tests/Scripts/links.py
+++ Tests/Scripts/links.py
@@ -9,20 +9,30 @@ class testing(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         browser.get("localhost:4200")
-        browser.implicitly_wait(5)
+        browser.implicitly_wait(10)
     @classmethod
     def tearDownClass(self):
         browser.quit()
 
-
     def test_linkNumber(self):
         links = browser.find_elements(By.TAG_NAME,"a")
         no_links = len(links)
-        print("Number of links: ")
-        print(no_links)
-        print()
+        file = open("/home/merima/ChainiumScanTesting/Reports/linkNumber.txt","w+")
+        file.write("Number of links: %d \n" %no_links)
+        file.write("Existing texts of links: \n")
+        text_no = 0
         for link in links:
-            print(link.text)
+            if link.text != "":
+                text_no = text_no + 1
+            file.write(link.text)
+            file.write("\n")
+        
+        #need to check how many links have texts
+        if text_no == no_links:
+            file.write("All links have texts")
+        else:
+            file.write("NUMBER OF LINKS THAT DOES NOT HAVE TEXT: %d" %(no_links-text_no))
+        file.close()
 
 
 
