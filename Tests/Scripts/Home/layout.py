37ee636fe9dec19273b68fffdd01422982fcf1e1
merimakopic <merima.kopic@fet.ba>
2019-08-07 23:28:36 +0200

    Update

--- Tests/Scripts/Home/layout.py
+++ Tests/Scripts/Home/layout.py
@@ -2,16 +2,14 @@ import unittest
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 
 browser = webdriver.Chrome()
 
 class testingHome(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        options = Options()
-        options.add_argument("--kiosk")
         browser.get("localhost:4200")
+        browser.maximize_window
         browser.implicitly_wait(3)
         time.sleep(4)
     @classmethod
@@ -20,7 +18,6 @@ class testingHome(unittest.TestCase):
 
     def test_screenshot(self):
         browser.save_screenshot("/home/merima/ChainiumScanTesting/Reports/Home/layout.png")
-
 
 if __name__ == "__main__":
     unittest.main()
